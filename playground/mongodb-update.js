e1cc9d5c6cc0476a49d6a1abf41bfea729f80844
bnt <bnt@tutanota.com>
2017-11-19 19:06:54 +0000

    extra changes to playground update

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -12,9 +12,17 @@ MongoClient.connect('mongodb://localhost:27017/node', (err, db) => {
 
     db.co
 
-    db.collection('todo').findOneAndUpdate(
-      {_id: new ObjectID('5a0b433c532933b6885205d7')},
-      {$set: {completed: true}},
+    // db.collection('todo').findOneAndUpdate(
+    //   {_id: new ObjectID('5a0b433c532933b6885205d7')},
+    //   {$set: {completed: true}},
+    //   {returnOriginal: false}
+    // ).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('users').findOneAndUpdate(
+      {_id: new ObjectID('5a0b4ab1532933b6885206a8')},
+      {$set: {name: 'ken'}, $inc: {age: 1}},
       {returnOriginal: false}
     ).then((result) => {
         console.log(result);
